caa98db0d4f4afcc701208ad97cf7affcf520793
OVERENSAL <mle8@yandex.ru>
2021-02-25 09:08:52 +0300

    added 1 lab

---
+++ Valuator/Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Valuator.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IStorage _storage;
+
+        public IndexModel(IStorage storage)
+        {
+            _storage = storage;
+        }
+
+        public IActionResult OnPost(string text)
+        {
+            string id = Guid.NewGuid().ToString();
+
+            string similarityKey = "SIMILARITY-" + id;
+            _storage.Store(similarityKey, GetSimilarity(text, id).ToString());
+            //TODO: посчитать similarity и сохранить в БД по ключу similarityKey
+
+            string textKey = "TEXT-" + id;
+            _storage.Store(textKey, text);
+
+            string rankKey = "RANK-" + id;
+            _storage.Store(rankKey, GetRank(text));
+            //TODO: посчитать rank и сохранить в БД по ключу rankKey
+
+            return Redirect($"summary?id={id}");
+        }
+
+        private int GetSimilarity(string text, string id)
+        {
+            var keys = _storage.GetKeys();
+
+            return keys.Any(item => item.Substring(0, 5) == "TEXT-" && _storage.Load(item) == text) ? 1 : 0;
+        }
+
+        public string GetRank(string text)
+        {
+            double nonLetterCount = text.Count(x => !char.IsLetter(x));
+
+            return ((double)(nonLetterCount / text.Length)).ToString();
+        }
+    }
+}
---
+++ Valuator/Storage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using StackExchange.Redis;
+
+namespace Valuator
+{
+    public class Storage : IStorage
+    {
+        private const string Host = "localhost";
+        private const int Port = 6379;
+        private readonly IConnectionMultiplexer _connection;
+ 
+        public Storage()
+        {
+            _connection = ConnectionMultiplexer.Connect(Host);
+        }
+
+        public void Store(string key, string text)
+        {
+            var db = _connection.GetDatabase();
+            db.StringSet(key, text);
+        }
+
+        public string Load(string key)
+        {
+            var db = _connection.GetDatabase();
+            return db.StringGet(key);
+        }
+
+        public List<string> GetKeys()
+        {
+            var keys = _connection.GetServer(Host, Port).Keys();
+
+            return keys.Select(item => item.ToString()).ToList();
+        }
+    }
+}
