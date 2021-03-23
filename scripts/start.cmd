9be80db414e3be5aa11ec570e6a912807a759208
Nickolay Dudin <42511187+OVERENSAL@users.noreply.github.com>
2021-03-23 20:06:53 +0300

    took out sctipts and nginx

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -1,7 +1,12 @@
 cd ..
+
+cd Valuator
+
 start dotnet run --urls "http://localhost:5001/"
 start dotnet run --urls "http://localhost:5002/"
 
-start nginx-1.19.8\ nginx.exe
+cd..
+
+start nginx-1.19.8\nginx.exe
 
 pause
