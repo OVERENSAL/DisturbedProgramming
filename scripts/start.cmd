1429bf1576e3bb1e52d77682d234baf223e71a6e
Nickolay Dudin <42511187+OVERENSAL@users.noreply.github.com>
2021-06-18 10:14:56 +0300

    fixed

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -1,12 +1,10 @@
-cd ..
+cd..
+cd nginx
+start nginx.exe
 
+cd ..
 cd Valuator
 
 start dotnet run --urls "http://localhost:5001/"
 start dotnet run --urls "http://localhost:5002/"
 
-cd..
-
-start nginx\nginx.exe
-
-pause
